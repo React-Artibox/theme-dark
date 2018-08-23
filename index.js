b3c553068a8b6b22337ae0ebe0bb5f516a639069
Chia Yu Pai <fantasyatelier@gmail.com>
2018-08-23 20:19:58 +0800

    initial project

---
+++ index.js
@@ -0,0 +1,12 @@
+const Color = require('@artibox/colors').Color;
+
+Color.addThemeMap('dark', {
+  BLOCK_ICON_HOVERED: '#212121',
+  EDITOR_BACKGROUND: 'transparent',
+  EDITOR_BORDER: '#797979',
+  BLOCK_ICON: '#757575',
+  INPUT_BACKGROUND: '#f0f0f0',
+  INPUT_BACKGROUND_HOVERED: '#e2e2e2',
+  INPUT_BACKGROUND_HOVERED_MASK: 'rgba(0, 0, 0, 0.3)',
+  INPUT_BORDER: 'transparent',
+});
